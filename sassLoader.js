5aa39c315d55b65b63207ed16db66b010fae4b98
Kristijan <kristijan.pajtasev@hotmail.com>
2016-10-01 02:18:58 +0100

    working with subfolders

--- sassLoader.js
+++ sassLoader.js
@@ -1,25 +1,9 @@
 module.exports = function(source) {
 
-    var css = "body { background: black; color: white; }";
-    var styleTag = `'<style type="text/css">${css}</style>'`;
-
     var sass = require('node-sass');
     var res = sass.renderSync({
-        data: source
+        file: this.resourcePath
     }).css.toString().replace(/\n/gi, "");
-    console.log(res)
-    //var link = document.createElement("link")
-    //undefined
-    //link.innerText
-    //""
-
-    //var head = document.querySelector("head");
-    //var style = document.createElement("style");
-    //style.innerText = css;
-    //style.type = "text/javascript";
-    //head.appendChild(style);
-
-    //  var style = document.createElement("style"); style.innerText = ${css}; style.type = "text/javascript"; head.appendChild(style);
 
     return `(function() { console.log("22");` +
         `var head = document.querySelector("head"); ` +
